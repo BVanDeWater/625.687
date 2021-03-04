4383487e27ae059eb0474adb02c416b78fd70bac
Tim <timothyfdavison@gmail.com>
2021-03-03 21:26:57 -0500

    Combinatorial (slow) finding of all possible linear combinations of pairs, triplets mod 2

--- Homework 4/main.py
+++ Homework 4/main.py
@@ -1,6 +1,4 @@
-
 from src import AbstractSimplicialComplex, dataset_hw3, utils
-
 
 
 if __name__ == "__main__":
@@ -11,14 +9,24 @@ if __name__ == "__main__":
     vectorsA, mappingA = utils.build_custom_complex(datasetA)
     vectorsB, mappingB = utils.build_custom_complex(datasetB)
 
-    # Testing out pairwise vector addition mod 2
-    sample_v1 = vectorsA[0]
-    sample_v2 = vectorsA[1]
-    sample_v3 = utils.mod2_vector_addition(sample_v1, sample_v2)
-    print(sample_v1, ": vector #1")
-    print(sample_v2, ": vector #2")
-    print(sample_v3, ": vector #1 + vector #2, mod 2")
+    # Generating all possible combinations of pairs/ triples mod 2 for dataset A
+    dim2_boundariesA = utils.compute_pchain_boundaries(vectorsA, 2)
+    dim3_boundariesA = utils.compute_pchain_boundaries(vectorsA, 3)
+    num_pairsA = len([x for x in vectorsA if sum(x) == 2])
+    num_triplesA = len([x for x in vectorsA if sum(x) == 3])
+    print("Dataset A: generated {} unique elements from {} boundary pairs"
+          .format(len(dim2_boundariesA), num_pairsA))
+    print("Dataset A: generated {} unique elements from {} boundary triplets"
+          .format(len(dim3_boundariesA), num_triplesA))
 
-    # Testing out batch vector addition mod 2
-    full_sum = utils.mod2_n_vector_addition(vectorsA)
-    print(full_sum, ": dataset A, summed pairwise")
+    # Generating all possible combinations of pairs/ triples mod 2 for dataset B
+    # User beware - slow!! Combinatorial explosion - not a scalable solution for finding images
+    dim2_boundariesB = utils.compute_pchain_boundaries(vectorsB, 2)
+    dim3_boundariesB = utils.compute_pchain_boundaries(vectorsB, 3)
+    num_pairsB = len([x for x in vectorsB if sum(x) == 2])
+    num_triplesB = len([x for x in vectorsB if sum(x) == 3])
+    print(
+        "Dataset B: generated {} unique elements from {} boundary pairs"
+        .format(len(dim2_boundariesB), num_pairsB))
+    print("Dataset G: generated {} unique elements from {} boundary triplets"
+          .format(len(dim3_boundariesB), num_triplesB))
--- src/utils.py
+++ src/utils.py
@@ -1,14 +1,15 @@
 ###########
 # PARSERS #
 ###########
+from itertools import combinations
 import numpy as np
 
 from .AbstractSimplicialComplex import AbstractSimplicialComplex
 
 
-
 string_to_smplcs = lambda st: [smplx.replace("}", "") for smplx in st.split("}, ")]
 smplcs_to_cmplx  = lambda smplcs: [smplx.replace("{", "").replace(" ", "").split(',') for smplx in smplcs]
+
 
 def build_custom_complex(cmplx):
     """
@@ -33,17 +34,35 @@ def mod2_vector_addition(v1, v2):
     v3_mod_2 = np.mod(v3, mod_2)
     return v3_mod_2
 
+
 def mod2_n_vector_addition(data):
     """
     Take a list of N vectors, compute their sum mod 2
 
     """
+    data = np.asarray(data)
     for i in range(len(data)):
         if not isinstance(data[i], np.ndarray):
             data[i] = np.asarray(data[i])
+    if len(data) < 2:
+        return []
     v1 = data[0]
     for i in range(1, len(data)):
         v1 += data[i]
     mod_2 = [2]*len(v1)
     v2_mod_2 = np.mod(v1, mod_2)
     return v2_mod_2
+
+
+def compute_pchain_boundaries(data, starting_dim=2):
+    """
+    Compute and return boundaries for n-dimensional p-chains
+
+    """
+    data = [x for x in data if sum(x) == starting_dim]  # assumes one-hot encoding
+    image = set()
+    for i in range(1, len(data)):
+        i_length_combos = combinations(data, i)
+        for combo in i_length_combos:
+            image.add(tuple(mod2_n_vector_addition(combo)))
+    return image
