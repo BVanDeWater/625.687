e6c319c87abae3117bfe0ac5bea0550889dfcd37
Tim <timothyfdavison@gmail.com>
2021-04-14 20:26:22 -0400

    Adding spectral correlation pseudometric

--- src/Metrics.py
+++ src/Metrics.py
@@ -50,6 +50,21 @@ class Metric:
     def dot_product(self, x, y):
         return np.dot(x, y)
 
+    def spectral_correlation_pseudometric(self, matrix_x, matrix_y):
+        """
+        Correlation pseudometric defined between two matrices; formula
+        from https://asa.scitation.org/doi/pdf/10.1121/1.5027825
+        (might need to be logged in to JHU for the link to work)
+        """
+        x2 = sum([matrix_x[i][j]**2 for i in range(len(matrix_x))
+                  for j in range(len(matrix_x[0]))]) ** (1/2)
+        y2 = sum([matrix_y[i][j]**2 for i in range(len(matrix_y))
+                  for j in range(len(matrix_y[0]))]) ** (1/2)
+        matrix_sum = sum([matrix_x[i][j]*matrix_y[i][j] for
+                          i in range(len(matrix_x))
+                          for j in range(len(matrix_x[0]))])
+        m = 1 - (matrix_sum / (x2 * y2) )
+        return m
 
 
 sample_vector1 = [0, 0, 1, 2, 0.5, 0]
@@ -62,3 +77,4 @@ sample_matrix2 = [sample_vector3, sample_vector4]
 
 metric = Metric()
 print("L2 norm:", metric.cosine_similarity(sample_vector1, sample_vector2))
+print("Pseduom:", metric.spectral_correlation_pseudometric(sample_matrix1, sample_matrix2))
